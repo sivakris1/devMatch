9df22d80afaa345835871fe48f3f8ae771887702
sivakrish1 <sivapalaparthi003@gmail.com>
2025-10-07 17:42:16 +0530

    Completed developers backend and all end points

--- backend/routes/developers.js
+++ backend/routes/developers.js
@@ -69,3 +69,96 @@ router.get('/recommend',auth, async(req,res)=>{
     res.status(500).json({ message: 'Server error' });
   }
 })
+
+
+//Searching Users based on skills, experience, location
+router.post('/search',auth,async(req,res) => {
+  try {
+    const {skills, experienceLevel, location, page=1, limit=1}
+
+    if(!skills && !experienceLevel && !location){
+      return res.status(400).json({message : "Atleast One Field is required"})
+    }
+
+    // Calculate pagination
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+    const limitNum = parseInt(limit);
+
+    const query = {
+      _id : {$ne : req.user._id}
+    }
+
+    if(skills){
+      query.skills = skills
+    }
+
+    if(experienceLevel){
+      query.experienceLevel = experienceLevel
+    }
+
+    if(location){
+      query.location = location
+    }
+
+    const developers = await User.find(query).select('-password -email').sort({createdAt : -1}).skip(skip).limit(limitNum)
+
+    const totalCount = await User.countDocuments(query);
+    const totalPages = Math.ceil(totalCount / limitNum);
+
+      res.json({
+      success: true,
+      data: {
+        developers,
+        pagination: {
+          currentPage: parseInt(page),
+          totalPages,
+          totalCount,
+          hasNext: parseInt(page) < totalPages,
+          hasPrev: parseInt(page) > 1
+        }
+      }
+    });
+
+  } catch (error) {
+    console.error('Search developers error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+})
+
+
+//searching all the Users
+router.get('/',auth,async(req,res) => {
+  try {
+    const {page=1, limit=10} = req.query
+
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+    const limitNum = parseInt(limit);
+
+    const developers = await User.find({
+      _id : {$ne : req.user._id}
+    }).select('-password -email').sort({createdAt : -1}).skip(skip).limit(limit)
+
+    const totalCount = await User.countDocuments({_id : {$ne : req.user._id}})
+
+    const totalPages = Math.ceil(totalCount / limitNum);
+
+    res.json({
+      success: true,
+      data: {
+        developers,
+        pagination: {
+          currentPage: parseInt(page),
+          totalPages,
+          totalCount,
+          hasNext: parseInt(page) < totalPages,
+          hasPrev: parseInt(page) > 1
+        }
+      }
+    });
+  } catch (error) {
+    console.error('Get developers error:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+})
+
+export default router;
