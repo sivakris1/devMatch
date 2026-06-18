a26b42cd9d7490cd394df73b46270e1a71b11d41
sivakrish1 <sivapalaparthi003@gmail.com>
2026-06-18 20:43:29 +0530

    refactor: add property to schema for storing cloudinary link in profile avatar

--- backend/models/User.js
+++ backend/models/User.js
@@ -60,6 +60,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    avatar: {
+      type: String,
+      default: '', 
+    },
   },
   {
     timestamps: true,
